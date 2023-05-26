516cb77f9c1b07fd1750097786b95207eae95b84
Steve Hayman <shayman@apple.com>
2023-05-26 14:32:20 -0400

    Change allSoundURLs to inculde module as well

--- STREAK+TestApp/ContentView.swift
+++ STREAK+TestApp/ContentView.swift
@@ -6,14 +6,12 @@
 //
 
 import SwiftUI
+import STREAK
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ForEach(allSoundFiles()) { SoundButton($0)}
         }
         .padding()
     }
