efec3a821ce7fb3461fd714d6b23f774245da2c4
Steve Hayman <shayman@apple.com>
2023-05-26 14:06:39 -0400

    Initial Commit

---
+++ STREAK+TestApp/STREAK_TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  STREAK_TestAppApp.swift
+//  STREAK+TestApp
+//
+//  Created by Steve Hayman on 2023-05-26.
+//
+
+import SwiftUI
+
+@main
+struct STREAK_TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
